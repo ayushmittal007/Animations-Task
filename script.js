a695f1055ef8fa08250d09fd2ecf58574ef8db02
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-10-01 17:12:22 +0530

    Added Basic CSS and JS Animation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Ayush Agrawal");
+const text = "Hello, World!";
+const outputElement = document.getElementById("output");
+
+function displayLetters(text) {
+  let index = 0;
+  const interval = setInterval(() => {
+    if (index < text.length) {
+      outputElement.textContent += text[index];
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 240); 
+}
+
+displayLetters(text);
