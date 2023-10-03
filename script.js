865223c94937201e7cfa46585c3eaf15e723f815
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-10-03 14:23:30 +0530

    Added GameOver File

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// let arr = ["Hello Ayush" , "Hello Aman", "Hello Aryan" , "Hello Aviral","Hello Jatin"]
-
-// function getRandomInt(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min)) + min;
-// }
-// let idx = getRandomInt(0,5)
-
 const text = "WELCOME";
 const outputElement = document.getElementById("output");
 
